17ce40b3f9e8a0b32c0a892bcf0dd2d045b2aae9
erosfrancesco <eros.francesco.francesco@gmail.com>
2024-03-04 12:13:15 +0100

    fix import

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from "url";
 import preact from '@preact/preset-vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  resolve: {
+    alias: [
+      { find: 'interfaces', replacement: fileURLToPath(new URL('./src/interfaces', import.meta.url)) },
+    ],
+  },
 })
