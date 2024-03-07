746a5bef04ab60ffb82cd2847557c25276abd621
erosfrancesco <eros.francesco.francesco@gmail.com>
2024-03-07 15:47:35 +0100

    clean up

--- src/logic/ws.ts
+++ src/logic/ws.ts
@@ -1,12 +1,10 @@
 import { WSMessageStore, WSMessageStoreState } from 'interfaces/ws';
 import { create } from 'zustand'
 
-
 const initialState :WSMessageStoreState = {
     pool: []
 };
 
-// TODO: - On WS Connected
 export const useWSMessages = create<WSMessageStore>((set) => ({
     ...initialState,
     save: (newMessage) => set((state) => {
