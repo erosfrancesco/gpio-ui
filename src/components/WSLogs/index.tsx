68b072fc20bc174014684ada657ec1f47b449b59
erosfrancesco <eros.francesco.francesco@gmail.com>
2024-03-07 13:17:31 +0100

    WSLog compact

--- src/components/WSLogs/index.tsx
+++ src/components/WSLogs/index.tsx
@@ -1,11 +1,12 @@
 import { useWSMessages } from 'logic/ws';
 import './index.css'
 import { event } from 'ws/index';
-import { useEffect } from 'preact/hooks';
+import { useEffect, useState } from 'preact/hooks';
 const { VITE_WS_ACTION_SENDMESSAGE } = import.meta.env;
 
 
 function WSLogs() {
+    const [isCompressed, setIsCompressed] = useState(false);
     const wsMessages = useWSMessages();
     useEffect(() => {
         event.on(VITE_WS_ACTION_SENDMESSAGE, (message) => {
@@ -13,8 +14,11 @@ function WSLogs() {
         });
     }, []);
     
+    const toggleCompressed = () => {
+        setIsCompressed(!isCompressed)
+    };
 
-    return <div className='wslog_wrapper'>
+    return <div className={'wslog_wrapper' + (isCompressed ? ' wslog_wrapper_compressed' : '')} onClick={toggleCompressed}>
         {wsMessages.pool.map((message) => <p className='wslog_row'>{message}</p>)}
     </div>
 }
